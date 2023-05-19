36b9a5d5eb4844c87a52a72b9bf5f3517e717372
Marisol <marisol.azofeifa@totemtech.net>
2023-05-19 17:33:35 -0600

    cambios

--- js/section-hero.js
+++ js/section-hero.js
@@ -2,6 +2,28 @@ $(document).ready(pagina_lista());
 
 function pagina_lista() {
 
+  const sectionHero = document.getElementById('hero-section');
+  const circle = document.getElementById('circle');
+
+  sectionHero.addEventListener('mouseenter', () => {
+    document.addEventListener('mousemove', moveCircle);
+    circle.style.display = 'block';
+    console.log('entre');
+  });
+
+  sectionHero.addEventListener('mouseleave', () => {
+    document.removeEventListener('mousemove', moveCircle);
+    circle.style.display = 'none';
+    console.log('sali');
+  });
+
+  function moveCircle(event) {
+    const x = event.clientX - circle.offsetWidth / 2;
+    const y = event.clientY - circle.offsetHeight / 2;
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+  }
+
   $("#texto-md").delay(1000).animate({top: "0px"}, 800);
   $("#texto-bg").delay(1000).animate({bottom: "0px"}, 800);
 
@@ -35,27 +57,6 @@ function pagina_lista() {
     });
   });
 
-  const sectionHero = document.getElementById('hero-section');
-  const circle = document.getElementById('circle');
-
-  sectionHero.addEventListener('mouseenter', () => {
-    document.addEventListener('mousemove', moveCircle);
-    circle.style.display = 'block';
-    console.log('entre');
-  });
-
-  sectionHero.addEventListener('mouseleave', () => {
-    document.removeEventListener('mousemove', moveCircle);
-    circle.style.display = 'none';
-    console.log('sali');
-  });
-
-  function moveCircle(event) {
-    const x = event.clientX - circle.offsetWidth / 2;
-    const y = event.clientY - circle.offsetHeight / 2;
-    circle.style.left = x + 'px';
-    circle.style.top = y + 'px';
-  }
 }
 
   
